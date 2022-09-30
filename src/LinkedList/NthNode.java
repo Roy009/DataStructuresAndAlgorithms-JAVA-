a58d687d18ee0e7153ab94bac0a5cbda66bcc165
Rohit009 <63160366+Roy009@users.noreply.github.com>
2022-09-30 21:47:23 +0530

    NthNode in Linked list

--- src/LinkedList/NthNode.java
+++ src/LinkedList/NthNode.java
@@ -2,6 +2,40 @@ package LinkedList;
 
 public class NthNode {
     public static void main(String[] args) {
+        linkedList l = new linkedList();
+        l.push(5);
+        l.push(1);
+        l.push(2);
+        l.push(3);
+        l.push(4);
+        System.out.println("The last Element is " + l.getNthNode());
+    }
+}
+class Node{
+    int data;
+    Node next;
+    Node(int d){
+        data = d;
+        next = null;
+    }
+}
 
+class linkedList{
+    Node head;
+
+    public void push(int data){
+        Node new_node = new Node(data);
+        new_node.next = head;
+        head = new_node;
+    }
+    public int getNthNode(){
+        Node temp = head;
+        while (temp != null){
+            if (temp.next == null){
+                return temp.data;
+            }
+            temp = temp.next;
+        }
+        return 0;
     }
 }
