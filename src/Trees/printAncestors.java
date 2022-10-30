e1b793d99fcd7c82d3951d0530a46ccd71572f3d
Rohit009 <63160366+Roy009@users.noreply.github.com>
2022-10-30 21:03:15 +0530

    printing Ancestors of a given node

---
+++ src/Trees/printAncestors.java
@@ -0,0 +1,35 @@
+package Trees;
+
+public class printAncestors {
+    private Node root;
+    private static class Node{
+        int key;
+        Node left, right;
+        Node(int k){
+            key = k;
+            left = right = null;
+        }
+    }
+    printAncestors(){root = null;}
+    boolean printAncestorsOfANode(Node node, int target){
+        if (node == null)
+            return false;
+        if (node.key == target){
+            return true;
+        }
+        if (printAncestorsOfANode(node.left,target) || printAncestorsOfANode(node.right, target)){
+            System.out.print(node.key + " ");
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        printAncestors pa = new printAncestors();
+        pa.root = new Node(1);
+        pa.root.left = new Node(2);
+        pa.root.right = new Node(3);
+        pa.root.left.left = new Node(4);
+        pa.root.left.right = new Node(5);
+        pa.root.left.left.left = new Node(6);
+        pa.printAncestorsOfANode(pa.root, 6);
+    }
+}
