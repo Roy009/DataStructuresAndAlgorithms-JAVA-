e3717857eadba98963247605691fbc9ede02d6b6
Rohit009 <63160366+Roy009@users.noreply.github.com>
2023-04-29 20:14:29 +0530

    valid sudoku

---
+++ src/LeetCodeQuestion/LeetCode_36_ValidSudoku.java
@@ -0,0 +1,40 @@
+package LeetCodeQuestion;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LeetCode_36_ValidSudoku {
+
+    // method - 1
+    public static boolean isValidSudoku(char[][] board){
+        Set<String> set = new HashSet<>();
+        for (int row = 0; row < 9; row++) {
+            for (int column = 0; column < 9; column++) {
+                char number = board[row][column];
+                if(number != '.'){
+                    if (!set.add(number + "in row " + row) || !set.add(number + "in column " + column) || !set.add(number + "is in block " + (row/3) + "," + (column/3))){
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+
+    }
+    public static void main(String[] args) {
+        // test
+        char[][] board = {
+                {'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+                {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+                {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+                {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+                {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+                {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+                {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+                {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+                {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
+        };
+        boolean result = isValidSudoku(board);
+        System.out.println(result);
+    }
+}
