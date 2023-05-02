9655c1947445efef81dd70006248215bbfcb73f2
Rohit009 <63160366+Roy009@users.noreply.github.com>
2023-05-02 10:59:21 +0530

    LeetCode day-2 may

---
+++ src/LeetCodeQuestion/LeetCode_1822_signoftheproductofanarray.java
@@ -0,0 +1,20 @@
+package LeetCodeQuestion;
+
+public class LeetCode_1822_signoftheproductofanarray {
+    public static int signOfProduct(int[] nums){
+        int sign = 1;
+        for (int num : nums) {
+            if(num == 0) {
+                return 0;
+            }
+            if(num < 0){
+                sign = -1 * sign;
+            }
+        }
+        return sign;
+    }
+    public static void main(String[] args) {
+        int[] nums = {-1,-2,-3,-4,1,2,3,4};
+        System.out.println(signOfProduct(nums));
+    }
+}
