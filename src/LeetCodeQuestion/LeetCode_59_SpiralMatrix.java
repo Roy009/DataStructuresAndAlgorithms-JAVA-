2c4530cbf0ec2c64b909e64f6bb94f0cd8e64a87
Rohit009 <63160366+Roy009@users.noreply.github.com>
2023-05-10 12:36:48 +0530

    Leetcode day-9

---
+++ src/LeetCodeQuestion/LeetCode_59_SpiralMatrix.java
@@ -0,0 +1,36 @@
+package LeetCodeQuestion;
+
+public class LeetCode_59_SpiralMatrix {
+    public static int[][] spiralMatrix2(int n){
+        int[][] result = new int[n][n];
+        int count = 1;
+        for (int layer = 0; layer < (n+1)/2; layer++) {
+            // Left to Right
+            for (int i = layer + 1; i < n-layer; i++) {
+                result[layer][i] = count++;
+            }
+            // Top to Bottom
+            for (int i = layer + 1; i < n-layer; i++) {
+                result[i][n-layer-1] = count++;
+            }
+            // Right to Left
+            for (int i = layer+1; i < n-layer; i++) {
+                result[n-layer-1][n-i-layer] = count++;
+            }
+            // Bottom to Top
+            for (int i = layer+1; i < n-layer-1; i++) {
+                result[n-i-layer][layer] = count++;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = spiralMatrix2(3);
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.println(matrix[i][j]);
+            }
+        }
+    }
+}
