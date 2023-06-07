2dad4a3565651784d6ed14d7f8acaf24e5999753
Rohit009 <63160366+Roy009@users.noreply.github.com>
2023-06-07 12:11:45 +0530

    day-7 minimum flip to make AORB equal to 0

---
+++ src/LeetCodeQuestion/LeetCode_1318_MinimumFlipToMakeAORBEqual.java
@@ -0,0 +1,24 @@
+package LeetCodeQuestion;
+
+public class LeetCode_1318_MinimumFlipToMakeAORBEqual {
+    public static int minimumFlip(int a, int b, int c){
+        int answer = 0;
+        while(a != 0 || b != 0 || c != 0){
+            if((c&1) == 1){
+                if((a&1) == 0 && (b&1) == 0){
+                    answer++;
+                }
+            }else{
+                answer += (a&1) + (b&1);
+            }
+            a >>= 1;
+            b >>= 1;
+            c >>= 1;
+        }
+        return answer;
+    }
+    public static void main(String[] args) {
+        int a = 2, b = 6, c = 5;
+        System.out.println(minimumFlip(a,b,c));
+    }
+}
