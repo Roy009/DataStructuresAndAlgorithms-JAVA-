ff8c3922a1e63527789713e22bbe53eadda34260
Rohit009 <63160366+Roy009@users.noreply.github.com>
2023-07-11 22:31:42 +0530

    printing subsequences of an array whoose sum is equal to k

--- src/Recursion/SubArrayWithGivenSum.java
+++ src/Recursion/SubArrayWithGivenSum.java
@@ -4,10 +4,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SubArrayWithGivenSum {
-    public static void subArraySum(){
-
+    public static void helper(int index,List<Integer> result, int sum, int k, int[] arr){
+        if (index == arr.length){
+            if (k == sum){
+                System.out.println(result);
+            }
+            return;
+        }
+        result.add(arr[index]);
+        sum += arr[index];
+        helper(index + 1, result, sum, k, arr);
+        sum -= arr[index];
+        result.remove(result.size()-1);
+        helper(index + 1, result, sum, k, arr);
     }
     public static void main(String[] args) {
-
+        int[] arr = {1,2,1};
+        int k = 2;
+        ArrayList<Integer> ans = new ArrayList<>();
+        helper(0,ans, 0, k, arr);
     }
 }
