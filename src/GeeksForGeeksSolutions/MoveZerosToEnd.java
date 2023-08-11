f76dbed12fbd8aa204eeef469101b846e1d5e8f8
Rohit009 <63160366+Roy009@users.noreply.github.com>
2023-08-11 18:24:50 +0530

    move zeros to the end using swap

--- src/GeeksForGeeksSolutions/MoveZerosToEnd.java
+++ src/GeeksForGeeksSolutions/MoveZerosToEnd.java
@@ -3,7 +3,7 @@ package GeeksForGeeksSolutions;
 import java.util.Arrays;
 
 public class MoveZerosToEnd {
-    public static int[] moveZerosToEnd(int[] arr){
+    public static int[] moveZerosToEnd1(int[] arr){
         int count = 0;
         for (int i = 0; i < arr.length; i++) {
             if(arr[i] != 0){
@@ -15,8 +15,25 @@ public class MoveZerosToEnd {
         }
         return arr;
     }
+    public static int[] moveZerosToEnd2(int[] arr){
+        int j = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != 0){
+                swap(arr, j, i);
+                j++;
+            }
+        }
+        return arr;
+    }
+    public static void swap(int[] arr, int a, int b){
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
     public static void main(String[] args) {
-        int[] arr = {1,0,2,0,3,0};
-        System.out.println(Arrays.toString(moveZerosToEnd(arr)));
+        int[] arr1 = {1,0,2,0,3,0};
+        int[] arr2 = {1,0,2,0,3,0};
+        System.out.println(Arrays.toString(moveZerosToEnd1(arr1)));
+        System.out.println(Arrays.toString(moveZerosToEnd2(arr2)));
     }
 }
