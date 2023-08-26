e286cad9c0e12c32911227e4347314fc070c5812
Rohit009 <63160366+Roy009@users.noreply.github.com>
2023-08-26 15:34:39 +0530

    reorder an array element according to the index

--- src/GeeksForGeeksSolutions/ReorderAnArrayElementAccordingToIndexes.java
+++ src/GeeksForGeeksSolutions/ReorderAnArrayElementAccordingToIndexes.java
@@ -3,7 +3,7 @@ package GeeksForGeeksSolutions;
 import java.util.Arrays;
 
 public class ReorderAnArrayElementAccordingToIndexes {
-    public static int[] reorderArrayAccordingToIndexes(int[] arr, int[] index){
+    public static int[] reorderArrayAccordingToIndexes1(int[] arr, int[] index){
         int[] temp = new int[arr.length];
         for (int i = 0; i < arr.length; i++) {
             temp[index[i]] = arr[i];
@@ -14,10 +14,25 @@ public class ReorderAnArrayElementAccordingToIndexes {
         }
         return arr;
     }
+    static void swap(int[] arr, int a, int b){
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+    public static int[] reorderArrayAccordingToIndexes2(int[] arr, int[] index){
+        for (int i = 0; i < arr.length; i++) {
+            while(index[i] != i){
+                swap(arr, i, index[i]);
+                swap(index, i, index[i]);
+            }
+        }
+        return arr;
+    }
     public static void main(String[] args) {
         int[] arr = {11,10,12};
         int[] index = {1,0,2};
-        System.out.println(Arrays.toString(reorderArrayAccordingToIndexes(arr,index)));
+        System.out.println(Arrays.toString(reorderArrayAccordingToIndexes1(arr,index)));
+        System.out.println(Arrays.toString(reorderArrayAccordingToIndexes2(arr,index)));
 
     }
 }
