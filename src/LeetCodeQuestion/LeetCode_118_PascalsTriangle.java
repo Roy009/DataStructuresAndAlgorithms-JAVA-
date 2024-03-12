e54442fd36427c532ca81e91db4d006ea6a7eae5
Rohit009 <63160366+Roy009@users.noreply.github.com>
2024-03-12 12:53:11 +0530

    Optimization

--- src/LeetCodeQuestion/LeetCode_118_PascalsTriangle.java
+++ src/LeetCodeQuestion/LeetCode_118_PascalsTriangle.java
@@ -5,6 +5,26 @@ import java.util.Arrays;
 import java.util.List;
 
 public class LeetCode_118_PascalsTriangle {
+    // Approach - 2
+    public static List<Integer> generateRow(int row){
+        int ans = 1;
+        List<Integer> ansRow = new ArrayList<>();
+        ansRow.add(1);
+        for(int i = 1; i < row; i++){
+            ans = ans * (row - i);
+            ans = ans / i;
+            ansRow.add(ans);
+        }
+        return ansRow;
+    }
+    public static List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> ans = new ArrayList<>();
+        for(int i = 1; i <= numRows; i++){
+            ans.add(generateRow(i));
+        }
+        return ans;
+    }
+    // Approach - 2
     static List<List<Integer>>pascalTriangle(int numRows){
         List<List<Integer>> ans = new ArrayList<>();
         List<Integer> row, pre = null;
@@ -27,5 +47,6 @@ public class LeetCode_118_PascalsTriangle {
         int numRows = 5;
         ans = pascalTriangle(numRows);
         System.out.println(Arrays.toString(ans.toArray()));
+
     }
 }
