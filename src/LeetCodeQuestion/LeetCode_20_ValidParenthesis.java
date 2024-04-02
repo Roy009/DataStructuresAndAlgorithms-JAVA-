3fa14713e626ffd091f9acd3facfb07f11d71888
Rohit009 <63160366+Roy009@users.noreply.github.com>
2024-04-02 16:27:26 +0530

    Approach - 2 using stack

--- src/LeetCodeQuestion/LeetCode_20_ValidParenthesis.java
+++ src/LeetCodeQuestion/LeetCode_20_ValidParenthesis.java
@@ -1,5 +1,7 @@
 package LeetCodeQuestion;
 
+import java.util.Stack;
+
 public class LeetCode_20_ValidParenthesis {
     public static boolean validParenthesis1(String s){
         while(true) {
@@ -14,9 +16,26 @@ public class LeetCode_20_ValidParenthesis {
             }
         }
     }
+    public static boolean validParenthesis2(String s){
+        int n = s.length();
+        Stack<Character> st = new Stack<>();
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '['){
+                st.push(s.charAt(i));
+            }else{
+                if(st.isEmpty()) return false;
+                char ch = st.pop();
+                if ((s.charAt(i) != ')' && ch == '(') || (s.charAt(i) != '}' && ch == '{') || (s.charAt(i) != ']' && ch == '[')){
+                    return false;
+                }
+            }
+        }
+        return st.isEmpty();
+    }
 
     public static void main(String[] args) {
         String s = "()";
         System.out.println(validParenthesis1(s));
+        System.out.println(validParenthesis2(s));
     }
 }
